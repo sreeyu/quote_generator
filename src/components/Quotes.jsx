d58d546d06eeeac350efcd30f227bc706f619859
sreey <sreelakshmi12898@gmail.com>
2023-07-29 19:11:32 +0530

    Display quotes

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './Quotes.module.css';
 
 function Quotes(){
 
+    const [quotes, setQuotes] = useState([]);
+    const [quote, setQuote] = useState("");
+    
     useEffect(() => {
         fetch("https://type.fit/api/quotes")
         .then((response) => {
@@ -12,17 +15,26 @@ function Quotes(){
             return response.json();
         })
         .then((data) => {
-            console.log(data);
+            setQuotes(data);
+            setQuote(data[0]);
         })
         .catch((error) => {
             console.log(error)
         });
     }, []);
+    
+    const getQuote = () => {
+        let random = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[random]);
+    };
+
+    const author = quote.author?.includes('type.fit') ? quote.author?.substring(0, quote.author.indexOf(",") || quote.author.length) : quote?.author;
 
     return(
         <div className={styles["quote-container"]} >
-            <h3>The quote goes here and it keeps going</h3>
-            <button>New Quote!</button>
+            <h3>{quote.text}</h3>
+            <i>-{author}</i>
+            <button onClick={getQuote} >New Quote!</button>
         </div>
     );
 };
